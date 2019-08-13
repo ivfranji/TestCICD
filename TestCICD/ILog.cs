a0853a059991b2c011002b1d7790efc1127b781e
Ivan Franjic <ivfranji@microsoft.com>
2019-08-13 21:29:55 +0200

    Initial commit

---
+++ TestCICD/ILog.cs
@@ -0,0 +1,14 @@
+﻿namespace TestCICD
+{
+    /// <summary>
+    /// ILog interface.
+    /// </summary>
+    public interface ILog
+    {
+        /// <summary>
+        /// Log message.
+        /// </summary>
+        /// <param name="message">Message.</param>
+        void Log(string message);
+    }
+}
